943f6c0da97712efe5874a4c30ef6689494a2d9f
Flopylepk <florencia.torres@davinci.edu.ar>
2025-10-30 12:52:53 -0300

    .

--- app.py
+++ app.py
@@ -22,5 +22,9 @@ def pagina_no_encontrada(e):
 
 
 # este siempre va al final
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )
